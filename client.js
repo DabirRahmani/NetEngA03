02a53be622c234bdef032796257285cec3e07ab2
dabir rahmani <trtpta@gmail.com>
2024-11-23 11:03:34 +0330

    raw ws created in js. client and server

---
+++ client.js
@@ -0,0 +1,16 @@
+let socket = new WebSocket("ws://localhost:8080");
+
+document.forms.publish.onsubmit = function () {
+  let outgoingMessage = this.message.value;
+
+  socket.send(outgoingMessage);
+  return false;
+};
+
+socket.onmessage = function (event) {
+  console.log(event.data);
+  let message = event.data;
+  let messageElem = document.createElement("div");
+  messageElem.textContent = message;
+  document.getElementById("messages")?.prepend(messageElem);
+};
